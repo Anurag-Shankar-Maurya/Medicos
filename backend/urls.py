734e5e02969c1b938b10018fa9220fa1f515ad87
Anurag-Shankar-Maurya <131768157+Anurag-Shankar-Maurya@users.noreply.github.com>
2026-01-15 17:39:17 +0530

    Refactor URL patterns to specify user and medicine API routes for clarity

--- backend/urls.py
+++ backend/urls.py
@@ -21,8 +21,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('apps.users.urls')),
-    path('api/', include('apps.medicines.urls')),
+    path('api/users/', include('apps.users.urls')),
+    path('api/medicines/', include('apps.medicines.urls')),
     # your other urls...
 ]
 
